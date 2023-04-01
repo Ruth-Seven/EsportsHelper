b00d271da8234e3d47aa5b1ab7d02b77b07bd706
ove <ruth1998bigman@gmail.com>
2023-04-01 12:55:25 +0800

    fix: wait more when login

--- EsportsHelper/LoginHandler.py
+++ EsportsHelper/LoginHandler.py
@@ -8,30 +8,34 @@ from rich import print
 
 from EsportsHelper.util import TimeOutRetries
 
+
 class LoginHandler:
     def __init__(self, log, driver) -> None:
         self.log = log
         self.driver = driver
 
-    @TimeOutRetries(5, "登陆操作", "请检查网络和账号并重试") 
+    @TimeOutRetries(5, "登陆操作", "请检查网络和账号并重试")
     def automaticLogIn(self, username, password):
         loginButton = WebDriverWait(self.driver, 10).until(
             ec.presence_of_element_located((By.CSS_SELECTOR, "a[data-riotbar-link-id=login]")))
         self.driver.execute_script("arguments[0].click();", loginButton)
-        
+
         self.log.info("눈_눈 登录中...")
 
-        usernameInput = self.driver.find_element(By.CSS_SELECTOR, "input[name=username]")
-        usernameInput.send_keys(username)
-        passwordInput = self.driver.find_element(By.CSS_SELECTOR, "input[name=password]")
-        passwordInput.send_keys(password)
-        time.sleep(0.5) #wait for button
-        submitButton = self.driver.find_element(By.CSS_SELECTOR, "button[type=submit]")
+        WebDriverWait(self.driver, 20).until(
+            ec.presence_of_element_located((By.CSS_SELECTOR, "input[name=username]"))
+            ).send_keys(username)
+        self.driver.find_element(
+            By.CSS_SELECTOR, "input[name=password]").send_keys(password)
+
+        time.sleep(0.5)  # wait for button
+        submitButton = self.driver.find_element(
+            By.CSS_SELECTOR, "button[type=submit]")
         self.driver.execute_script("arguments[0].click();", submitButton)
         try:
-            WebDriverWait(self.driver, 15).until(ec.presence_of_element_located((By.CSS_SELECTOR, "div.riotbar-summoner-name")))
+            WebDriverWait(self.driver, 15).until(ec.presence_of_element_located(
+                (By.CSS_SELECTOR, "div.riotbar-summoner-name")))
             self.log.info("∩_∩ 账密 提交成功")
             return True
         except TimeoutException:
             return False
-        
